d20e500bf3dc85210592872fb993373fdd03a5ef
Streafen Rasel <54981146+streafenrasel@users.noreply.github.com>
2022-09-23 00:14:43 +0600

    Create AccessingInherrited.cpp

---
+++ AccessingInherrited.cpp
@@ -0,0 +1,154 @@
+#include<iostream>
+
+using namespace std;
+
+class A
+{
+    public:
+        A(){
+            callA = 0;
+        }
+    private:
+        int callA;
+        void inc(){
+            callA++;
+        }
+
+    protected:
+        void func(int & a)
+        {
+            a = a * 2;
+            inc();
+        }
+    public:
+        int getA(){
+            return callA;
+        }
+};
+
+class B
+{
+    public:
+        B(){
+            callB = 0;
+        }
+    private:
+        int callB;
+        void inc(){
+            callB++;
+        }
+    protected:
+        void func(int & a)
+        {
+            a = a * 3;
+            inc();
+        }
+    public:
+        int getB(){
+            return callB;
+        }
+};
+
+class C
+{
+    public:
+        C(){
+            callC = 0;
+        }
+    private:
+        int callC;
+        void inc(){
+            callC++;
+        }
+    protected:
+        void func(int & a)
+        {
+            a = a * 5;
+            inc();
+        }
+    public:
+        int getC(){
+            return callC;
+        }
+};
+
+
+class D : public A, public B, public C
+{
+
+    int val;
+    public:
+         D()
+         {
+             val = 1;
+         }
+
+         void update_val(int new_val)
+         {
+            while(new_val%2 == 0) {
+                A::func(val);
+                new_val /= 2;
+            }
+            while(new_val%3 == 0) {
+                B::func(val);
+                new_val /= 3;
+            }
+            while(new_val%5 == 0) {
+                C::func(val);
+                new_val /= 5;
+            }
+         }
+         //For Checking Purpose
+         void check(int); //Do not delete this line.
+};
+
+
+
+void D::check(int new_val)
+{
+    update_val(new_val);
+    cout << "Value = " << val << endl << "A's func called " << getA() << " times " << endl << "B's func called " << getB() << " times" << endl << "C's func called " << getC() << " times" << endl;
+}
+
+
+int main()
+{
+    D d;
+    int new_val;
+    cin >> new_val;
+    d.check(new_val);
+
+}
+/*
+________STREAFEN_0101_RASEL__
+________STREAFEN_0101_RASEL__
+_____________0101010_________
+_____________0101010_________
+_____________0101010_________
+_____________0101010_________
+_____________0101010_________
+_____________0101010_________
+_____________0101010_________
+_____________0101010_________
+________STREAFEN_0101_RASEL__
+________STREAFEN_0101_RASEL__
+
+_____010101_____________________
+_0101010101010__________________
+010101010101010_________________
+01010101010101011_______________
+01010101010101010_______________
+01010101010101010101010101______
+01010101010101010101010101010___
+0101010100101010101010101010101_
+01010101010101010101010101010101
+010101010101010101010101010__01_
+01010101010101010100101010101___
+01010100101010010101010101______
+01010101010101010101010_________
+1010101010101010101_____________
+01010101010101__________________
+101010101_______________________
+010101__________________________
+10______________________________
+*/
