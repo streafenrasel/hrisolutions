66a80d034faac1f6251ec7cb256d76a686986986
Streafen Rasel <54981146+streafenrasel@users.noreply.github.com>
2022-09-23 00:11:15 +0600

    Create ExeptionalServer.cpp

---
+++ ExeptionalServer.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <exception>
+#include <string>
+#include <stdexcept>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+class Server {
+private:
+	static int load;
+public:
+	static int compute(long long A, long long B) {
+		load += 1;
+		if(A < 0) {
+			throw std::invalid_argument("A is negative");
+		}
+		vector<int> v(A, 0);
+		int real = -1, cmplx = sqrt(-1);
+		if(B == 0) throw 0;
+		real = (A/B)*real;
+		int ans = v.at(B);
+		return real + A - B*ans;
+	}
+	static int getLoad() {
+		return load;
+	}
+};
+int Server::load = 0;
+
+int main() {
+	int T; cin >> T;
+	while(T--) {
+		long long A, B;
+		cin >> A >> B;
+try 
+        {
+            cout << Server::compute(A, B) << endl;
+        } 
+        catch (std::bad_alloc& error) 
+        {
+            cout << "Not enough memory" << endl;
+        }
+        catch (std::exception& error) 
+        {
+            cout << "Exception: " << error.what() << endl;
+        }
+        catch (...) 
+        {
+            cout << "Other Exception" << endl;
+        }
+		/* Enter your code here. */
+
+	}
+	cout << Server::getLoad() << endl;
+	return 0;
+}
+/*
+________STREAFEN_0101_RASEL__
+________STREAFEN_0101_RASEL__
+_____________0101010_________
+_____________0101010_________
+_____________0101010_________
+_____________0101010_________
+_____________0101010_________
+_____________0101010_________
+_____________0101010_________
+_____________0101010_________
+________STREAFEN_0101_RASEL__
+________STREAFEN_0101_RASEL__
+
+_____010101_____________________
+_0101010101010__________________
+010101010101010_________________
+01010101010101011_______________
+01010101010101010_______________
+01010101010101010101010101______
+01010101010101010101010101010___
+0101010100101010101010101010101_
+01010101010101010101010101010101
+010101010101010101010101010__01_
+01010101010101010100101010101___
+01010100101010010101010101______
+01010101010101010101010_________
+1010101010101010101_____________
+01010101010101__________________
+101010101_______________________
+010101__________________________
+10______________________________
+*/
