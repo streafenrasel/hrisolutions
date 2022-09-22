c5544d2807613a9da28f698c116306a2bbcc1e9d
Streafen Rasel <54981146+streafenrasel@users.noreply.github.com>
2022-09-23 00:07:13 +0600

    Create ClassesObjects.cpp

---
+++ ClassesObjects.cpp
@@ -0,0 +1,90 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+using namespace std;
+
+// Write your Student class here
+class Student 
+{
+    private:
+    int scores[5];
+    public:
+    void input()
+    {
+        for (int i = 0; i < 5; i++) 
+        {
+            cin >> scores[i];
+        }
+    }
+    int calculateTotalScore() 
+    {
+        int count = 0;
+        for (int i = 0; i < 5; i++) 
+        {
+            count += scores[i];
+        }
+        return count;
+    }
+};
+int main() {
+    int n; // number of students
+    cin >> n;
+    Student *s = new Student[n]; // an array of n students
+    
+    for(int i = 0; i < n; i++){
+        s[i].input();
+    }
+
+    // calculate kristen's score
+    int kristen_score = s[0].calculateTotalScore();
+
+    // determine how many students scored higher than kristen
+    int count = 0; 
+    for(int i = 1; i < n; i++){
+        int total = s[i].calculateTotalScore();
+        if(total > kristen_score){
+            count++;
+        }
+    }
+
+    // print result
+    cout << count;
+    
+    return 0;
+}
+/*
+________STREAFEN_0101_RASEL__
+________STREAFEN_0101_RASEL__
+_____________0101010_________
+_____________0101010_________
+_____________0101010_________
+_____________0101010_________
+_____________0101010_________
+_____________0101010_________
+_____________0101010_________
+_____________0101010_________
+________STREAFEN_0101_RASEL__
+________STREAFEN_0101_RASEL__
+
+_____010101_____________________
+_0101010101010__________________
+010101010101010_________________
+01010101010101011_______________
+01010101010101010_______________
+01010101010101010101010101______
+01010101010101010101010101010___
+0101010100101010101010101010101_
+01010101010101010101010101010101
+010101010101010101010101010__01_
+01010101010101010100101010101___
+01010100101010010101010101______
+01010101010101010101010_________
+1010101010101010101_____________
+01010101010101__________________
+101010101_______________________
+010101__________________________
+10______________________________
+*/
