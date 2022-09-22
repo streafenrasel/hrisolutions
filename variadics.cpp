9ec10c7e7432e67d455fc6fa33286d0c4c043fe9
Streafen Rasel <54981146+streafenrasel@users.noreply.github.com>
2022-09-23 00:19:14 +0600

    Create variadics.cpp

---
+++ variadics.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+// Enter your code for reversed_binary_value<bool...>()
+template <bool a> int reversed_binary_value() { return a; }
+
+template <bool a, bool b, bool... d> int reversed_binary_value() {
+  return (reversed_binary_value<b, d...>() << 1) + a;
+}
+
+
+
+
+
+template <int n, bool...digits>
+struct CheckValues {
+  	static void check(int x, int y)
+  	{
+    	CheckValues<n-1, 0, digits...>::check(x, y);
+    	CheckValues<n-1, 1, digits...>::check(x, y);
+  	}
+};
+
+template <bool...digits>
+struct CheckValues<0, digits...> {
+  	static void check(int x, int y)
+  	{
+    	int z = reversed_binary_value<digits...>();
+    	std::cout << (z+64*y==x);
+  	}
+};
+
+int main()
+{
+  	int t; std::cin >> t;
+
+  	for (int i=0; i!=t; ++i) {
+		int x, y;
+    	cin >> x >> y;
+    	CheckValues<6>::check(x, y);
+    	cout << "\n";
+  	}
+}
+/*
+________STREAFEN_0101_RASEL__
+________STREAFEN_0101_RASEL__
+_____________0101010_________
+_____________0101010_________
+_____________0101010_________
+_____________0101010_________
+_____________0101010_________
+_____________0101010_________
+_____________0101010_________
+_____________0101010_________
+________STREAFEN_0101_RASEL__
+________STREAFEN_0101_RASEL__
+
+_____010101_____________________
+_0101010101010__________________
+010101010101010_________________
+01010101010101011_______________
+01010101010101010_______________
+01010101010101010101010101______
+01010101010101010101010101010___
+0101010100101010101010101010101_
+01010101010101010101010101010101
+010101010101010101010101010__01_
+01010101010101010100101010101___
+01010100101010010101010101______
+01010101010101010101010_________
+1010101010101010101_____________
+01010101010101__________________
+101010101_______________________
+010101__________________________
+10______________________________
+*/
